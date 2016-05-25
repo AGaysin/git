85b326a805d0e465c00d30e4696947e7db3beacb
AG <Mayc@list.ru>
2016-05-25 15:52:15 +0500

    Отредактировано боковое и основное меню

--- android/CathodMon/app/src/main/java/tehohrana/ru/cathodmon/MainActivity.java
+++ android/CathodMon/app/src/main/java/tehohrana/ru/cathodmon/MainActivity.java
@@ -232,6 +232,14 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, Prefs.class);
+            startActivity(i);
+            return true;
+        }
+        else if (id==R.id.action_about)
+        {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
             return true;
         }
 
